6a9df9c33cc15d1d06a5b7b08e5e1342021e1637
Callum Macrae <callum@lynxphp.com>
2011-07-15 16:46:31 +0100

    Added copyright and got rid of the 5 thing.

--- js/game.js
+++ js/game.js
@@ -9,11 +9,9 @@ function move(time) {
 var c = 0, intv = setInterval(move, 1000, 500);
 $('#b').click(function() {
 	if ($(this).css('background-color') !== 'rgb(255, 255, 255)') {
-		$('span').css('background-color', 'white').text(++c);
-		if (c % 5 === 0) {
-			clearInterval(intv);
-			intv = setInterval(move, 1000 - c * 10, 500 - c * 5);
-			move(500 - c * 5);
-		}
+		$('span').text(++c);
+		clearInterval(intv);
+		intv = setInterval(move, 1000 - c * 10, 500 - c * 5);
+		move(500 - c * 5);
 	}
 });
